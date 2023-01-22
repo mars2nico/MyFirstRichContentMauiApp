2ba7d7a143a73e4ad94cd2d98b3ec90964d7c92e
Daichi Misaka <misaka6085@gmail.com>
2023-01-22 18:14:08 +0900

    プロジェクト ファイルを追加します。

---
+++ MyFirstRichContentMauiApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MyFirstRichContentMauiApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MyFirstRichContentMauiApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MyFirstRichContentMauiApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
